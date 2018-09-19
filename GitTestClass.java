5f8cde3c5ae8f90be590d89ef6c1b54a0cecc247
wexroot <enxin.wu@hand-china.com>
2018-09-19 22:02:23 +0800

    第二次使用IDEA提交到GitHub

--- GitTestClass.java
+++ GitTestClass.java
@@ -8,8 +8,10 @@ package com.test.git;
  */
 public class GitTestClass {
     public static void main(String[] args) {
-
-
-
+        System.out.println("------------------");
+        System.out.println("------------------");
+        System.out.println("------------------");
+        System.out.println("------------------");
+        System.out.println("------------------");
     }
 }
