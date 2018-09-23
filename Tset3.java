296ab6e76db1aa9517b4edcc0fb5612d27293977
wexroot <enxin.wu@hand-china.com>
2018-09-23 21:26:05 +0800

    第四次使用IDEA提交到GitHub,并进行合并

---
+++ Tset3.java
@@ -0,0 +1,20 @@
+package com.test.git;
+
+/**
+ * @Title: Tset3
+ * @Date: 2018/9/23 21:25
+ * @Author: enxin.wu
+ * @Description:
+ */
+public class Tset3 {
+    public static void main(String[] args) {
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+        System.out.println("------------Tset3--------------");
+    }
+}
