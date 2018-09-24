4ee05684bdf79829a9efdc1864ead00cdf4d540f
wexroot <enxin.wu@hand-china.com>
2018-09-24 10:36:52 +0800

    第五次使用IDEA提交到GitHub,并进行合并

---
+++ Test4.java
@@ -0,0 +1,18 @@
+package com.test.git;
+
+/**
+ * @Title: Test4
+ * @Date: 2018/9/24 10:34
+ * @Author: enxin.wu
+ * @Description:
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("---------Test4-----------");
+        System.out.println("---------Test4-----------");
+        System.out.println("---------Test4-----------");
+        System.out.println("---------Test4-----------");
+        System.out.println("---------Test4-----------");
+        System.out.println("---------Test4-----------");
+    }
+}
